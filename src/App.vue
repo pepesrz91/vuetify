e053ec137b8df66fd9daee4fe5859e50c10e989e
DESKTOP-2UG043F\pepe_ <pepesrzra@outlook.com>
2018-07-16 14:17:37 -0500

    Added Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <v-app>
+      <v-toolbar app>
+        <v-toolbar-title>My Application</v-toolbar-title>
+      </v-toolbar>
+      <v-navigation-drawer app></v-navigation-drawer>
+      <v-content>
+        <v-container fluid>
+          Hello World
+        </v-container>
+      </v-content>
+      <v-footer></v-footer>
+    </v-app>
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -28,6 +40,7 @@ export default {
     }
   }
 }
+
 </script>
 
 <style lang="scss">
