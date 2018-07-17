95a45a652f60be013027a909797a4479b109f9fb
DESKTOP-2UG043F\pepe_ <pepesrzra@outlook.com>
2018-07-16 19:23:56 -0500

    Got the app working with node.js ready for heroku deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/',(req,res)=>{
+  res.write('index.html');
+});
+
+
+app.listen(port,()=>{
+  console.log(`Server is up on port ${port}`);
+});
